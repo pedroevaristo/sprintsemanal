36d0f1fb28dfa5095b2c57f40edcf6097030930e
pedroevaristo <pedrodzprogramacao@gmail.com>
2024-10-10 23:12:34 -0300

    conseguir implementar a lógica de usar o controller para a view (cshtml)

---
+++ Controllers/RegisterUserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc.Controllers
+{
+    public class RegisterUserController : Controller
+    {
+        public IActionResult RegisterUser()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Register.cs
@@ -0,0 +1,9 @@
+﻿namespace mvc.Models
+{
+    public class Register
+    {
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String passwordAdmins { get; set; }
+    }
+}
