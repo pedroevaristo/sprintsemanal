513287f25cd3a968fc3a362cc752a12b2d5d8a07
mattktt <matheusmenezesramos@gmail.com>
2024-10-25 11:03:42 -0300

    alteração no controoller

--- Controllers/RegisterUserController.cs
+++ Controllers/RegisterUserController.cs
@@ -1,18 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 using mvc.Models;
 
 namespace mvc.Controllers
 {
     public class RegisterUserController : Controller
     {
+        private readonly IMongoCollection<Register> _users;
+        [HttpGet]
         public IActionResult RegisterUser()//Aqui tem como objetivo de repassar as informações de criar o cadastro
         {
             return View();
         }
-       /* public IActionResult ()
-        { //fazer a operação de cadastra as informações colcoando abaixo
-        }*/
-           
+        [HttpPost]
+        public IActionResult RegisterUser([Bind("Name", "Id")] Register model)// decorator
+        {
+            if (ModelState.IsValid)
+            {
+                _users.InsertOne(model);
+
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
 
     }
 }
